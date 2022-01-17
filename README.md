f80582d6d15a1de9f472bb91471ab8dbcb3a1500
John Garzon <gajohn511@gmail.com>
2022-01-16 21:15:28 -0500

    added deploy script, aws orb, aws setup, readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Main Branch Build
+
+[![jgarzonscript](https://circleci.com/gh/jgarzonscript/pipeline1.svg?style=svg)](https://app.circleci.com/pipelines/github/jgarzonscript/pipeline1)
+
+this repo is the main branch to the 2 child branches: (1) the API, (2) the website
--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,3 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+# aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ../build s3://jonsbucket333 --recursive 
+# pwd
